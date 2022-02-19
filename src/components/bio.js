b693085fff6660951fdc273661b7eca91b794bbe
Benjamin Speakman <benjaminspeakman@protonmail.ch>
2022-02-18 20:11:44 -0500

    remove default posts; add first post; fix dimens on icon

--- src/components/bio.js
+++ src/components/bio.js
@@ -33,8 +33,8 @@ const Bio = () => {
         layout="fixed"
         formats={["auto", "webp", "avif"]}
         src="../images/profile-pic.png"
-        width={50}
-        height={50}
+        width={48}
+        height={48}
         quality={95}
         alt="Profile picture"
       />
